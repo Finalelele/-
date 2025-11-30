c458eca442f8aba191f8702352b7b746e1f37a52
Finalelele <Akkountforgames@yandex.ru>
2025-11-30 22:49:42 +0500

    Update --.lua

--- --.lua
+++ --.lua
@@ -3,7 +3,10 @@ loadstring(game:HttpGet("https://pastebin.com/raw/DH4b3T5S"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Finalelele/craft-blocks-dupe-script/refs/heads/main/Dupe%20script.lua"))()
 
 local function esp()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Yahahahau/Ultimate-Esp-v1/refs/heads/main/Ultimate%20esp%20v1.lua"))()
+	while true do
+		wait(1)
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Yahahahau/Ultimate-Esp-v1/refs/heads/main/Ultimate%20esp%20v1.lua"))()
+	end
 end
 
 esp()
